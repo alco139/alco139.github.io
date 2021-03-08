45c6b917a23ed1a451f80df730cb94146eb4c6cf
Alan Dolan <alco139@gmail.com>
2021-03-08 23:57:54 +0100

    Patch 1.01 first page

---
+++ script.js
@@ -0,0 +1,19 @@
+// $(document).scroll(function () {
+//     var y = $(this).scrollBottom();
+//     if (y > 800) {
+//         $('.bottomMenu').fadeIn();
+//     } else {
+//         $('.bottomMenu').fadeOut();
+//     }
+
+// });
+$(function() { 
+    $("#one").addClass("progress-bar-orange");
+    $("#two").addClass("progress-bar-orange");
+    $("#three").addClass("progress-bar-orange");
+    $("#four").addClass("progress-bar-orange");
+    $("#five").addClass("progress-bar-orange");
+    $("#six").addClass("progress-bar-orange");
+    $("#seven").addClass("progress-bar-orange");
+    $("#eight").addClass("progress-bar-orange");
+ });
